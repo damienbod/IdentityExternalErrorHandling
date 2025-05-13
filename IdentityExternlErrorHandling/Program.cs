c14489e0e1e63969e282c4a17337fc8c1c79d4bb
damienbod <damien_bod@hotmail.com>
2025-05-13 21:33:27 +0200

    code clean up

--- IdentityExternlErrorHandling/Program.cs
+++ IdentityExternlErrorHandling/Program.cs
@@ -1,50 +1,86 @@
 using IdentityExternalErrorHandling.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace IdentityExternalErrorHandling
+namespace IdentityExternalErrorHandling;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddOpenIdConnect("EntraID", "EntraID", oidcOptions =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            oidcOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            oidcOptions.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+            oidcOptions.Scope.Add("user.read");
+            oidcOptions.Scope.Add(OpenIdConnectScope.OfflineAccess);
+            oidcOptions.Authority = $"https://login.microsoftonline.com/{builder.Configuration["AzureAd:TenantId"]}/v2.0/";
+            oidcOptions.ClientId = builder.Configuration["AzureAd:ClientId"];
+            oidcOptions.ClientSecret = builder.Configuration["AzureAd:ClientSecret"];
+            oidcOptions.ResponseType = OpenIdConnectResponseType.Code;
+            oidcOptions.MapInboundClaims = false;
+            oidcOptions.SaveTokens = true;
+            oidcOptions.TokenValidationParameters.NameClaimType = JwtRegisteredClaimNames.Name;
+            oidcOptions.TokenValidationParameters.RoleClaimType = "role";
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            oidcOptions.Events = new OpenIdConnectEvents
             {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                // Add event handlers            
+                OnMessageReceived = async context =>
+                {
+                    if (!string.IsNullOrEmpty(context.ProtocolMessage.Error))
+                    {
+                        context.HandleResponse();
+                        context.Response.Redirect($"/Error?remoteError={context.ProtocolMessage.Error}");
+                    }
 
-            app.UseHttpsRedirection();
+                    await Task.CompletedTask;
+                }
+            };
+        });
 
-            app.UseRouting();
+        var app = builder.Build();
 
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapRazorPages()
-               .WithStaticAssets();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
         }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+        app.MapRazorPages()
+           .WithStaticAssets();
+
+        app.Run();
     }
 }
